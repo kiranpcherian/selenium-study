6775ad651003304cc9d710b9a61b51c232710d77
Kiran Cherian <kiransara90@gmail.com>
2020-10-03 17:11:21 -0400

    Add test for Gender radio button. Closes #3

--- src/test/java/com/mycompany/seleniumstudy/tests/DemoTest.java
+++ src/test/java/com/mycompany/seleniumstudy/tests/DemoTest.java
@@ -30,6 +30,7 @@ public class DemoTest {
         WebDriver driver = new ChromeDriver();
         driver.get(baseUrl);
         WebElement txtSearch = driver.findElement(By.name("search_field"));
+        txtSearch.clear();
         txtSearch.sendKeys("table");
         WebElement btnSearch = driver.findElement(By.xpath("//*[@id=\"search\"]/form/div/span/input"));
         btnSearch.click();
